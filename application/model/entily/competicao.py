500f5f227e02939306f1ea36c74a932b030be8c9
Gabriel <tamiozzo98@gmail.com>
2021-06-23 18:46:37 -0300

    Criando a classe Competição

---
+++ application/model/entily/competicao.py
@@ -0,0 +1,22 @@
+from datetime import datetime
+from typing import List
+
+from application.model.entily.local import Local
+from application.model.entily.atleta import Atleta
+class Competicao():
+
+    def __init__(self,id:int ,nome: str, local: Local,data: datetime):
+        self.__id = id
+        self.__nome = nome
+        self.__local = local
+        self.__data = data
+
+    def get_id(self):
+        return self.__id
+
+    def get_nome(self):
+        return self.__nome
+    def get_local(self):
+        return self.__local
+    def get_data(self):
+        return self.__data
